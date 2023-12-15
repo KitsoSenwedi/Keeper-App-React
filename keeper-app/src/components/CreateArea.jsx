3f1a7526d589edd8ed480d8c6f6cfd94a25328c5
kitsosenwedi <boyboykitso@gmail.com>
2023-12-16 00:02:10 +0200

    complete

--- keeper-app/src/components/CreateArea.jsx
+++ keeper-app/src/components/CreateArea.jsx
@@ -1,18 +1,38 @@
 import React, {useState} from 'react';
 
-function CreateArea(){
+function CreateArea(props){
 
     const [note, setNote] = useState({
         title: "",
         content: "",
     })
 
+    function handleChange(event){
+       const {name, value} = event.target;
+
+       setNote(prevNote => {
+        return {
+            ...prevNote,
+            [name]: value
+        };
+       });
+    }
+
+    function submitNote(event) {
+        props.onAdd(note)
+        setNote({ 
+            title: "",
+            content: "",
+        })
+        event.preventDefault();
+    }
+
     return (
         <div>
             <form >
-                <input name="title" value={note.title} placeholder="Title" type="text"/>
-                <textarea name="content" value={note.content} placeholder="Take a note..." rows="3"></textarea>
-                <button>Add</button>
+                <input name="title" onChange={handleChange} value={note.title} placeholder="Title" type="text"/>
+                <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3"></textarea>
+                <button onClick={submitNote}>Add</button>
             </form>
         </div>
     )

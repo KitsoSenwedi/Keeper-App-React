4a4121cec1a8fbabb552087663160a835623e4ac
kitsosenwedi <boyboykitso@gmail.com>
2023-12-15 21:21:03 +0200

    not yet complete

---
+++ keeper-app/src/components/CreateArea.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+function CreateArea(){
+
+    const [note, setNote] = useState({
+        title: "",
+        content: "",
+    })
+
+    return (
+        <div>
+            <form >
+                <input name="title" value={note.title} placeholder="Title" type="text"/>
+                <textarea name="content" value={note.content} placeholder="Take a note..." rows="3"></textarea>
+                <button>Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default CreateArea;
